d838e2ddce768aba9e11e749de8ecd32391e81fe
prodhanm <mustafizur.prodhan@gmail.com>
2024-04-06 01:51:48 -0400

    modifed the increase/decrease item and delete item function

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:category_slug>/', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product, name='product'),
-    path('cart/', views.cart, name='cart',)
+    path('cart/add/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('cart/', views.cart, name='cart'),
+    path('cart/remove/<int:product_id>/', views.remove_cart, name='remove_cart'),
+    path('cart/delete/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Category, Product
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Category, Product, Cart, CartItem
 
 def home(request, category_slug=None):
     category_page = None
@@ -19,5 +20,69 @@ def product(request, category_slug, product_slug):
         raise e
     return render(request, 'product.html', {'product': product})
 # Create your views here.
+'''
 def cart(request):
     return render(request, 'cart.html')
+'''
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+            )
+        cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        if cart_item.quantity < cart_item.product.stock:
+            cart_item.quantity += 1
+            cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product, 
+            quantity=1, 
+            cart=cart
+            )
+        cart_item.save()
+    
+    return redirect('cart')
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist as e:
+        print(e)
+    return render(
+        request, 
+        'cart.html', 
+        dict(cart_items=cart_items, total=total, quantity=quantity)
+        )
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
