c8b85398248c4db31e852ee490a08de504570114
prodhanm <mustafizur.prodhan@gmail.com>
2024-04-16 12:25:53 -0400

    created the order models to align customer data

--- store/models.py
+++ store/models.py
@@ -67,3 +67,41 @@ class CartItem(models.Model):
     
     def __str__(self):
         return self.product
+    
+class Order(models.Model):
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Order Total')
+    email = models.EmailField(max_length=250, blank=True, verbose_name='Email')
+    created = models.DateTimeField(auto_now=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=10, blank=True)
+    billingCountry = models.CharField(max_length=250, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=10, blank=True)
+    shippingCountry = models.CharField(max_length=250, blank=True)
+    
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created']
+        
+    def __str__(self):
+        return str(self.id)
+    
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Price')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'OrderItem'
+        
+    def sub_total(self):
+        return self.price * self.quantity
+    
+    def __str__(self):
+        return self.product
