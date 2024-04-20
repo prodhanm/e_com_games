40d0c6eff6a7f0c4abe18f669c57f243573ba0b6
prodhanm <mustafizur.prodhan@gmail.com>
2024-04-20 07:56:46 -0400

    set up email settings

--- e_com_store/settings.py
+++ e_com_store/settings.py
@@ -141,6 +141,13 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+# Email settings
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
